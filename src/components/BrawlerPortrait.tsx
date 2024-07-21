8973df87955b06e3a87cef8d74d91f2070722620
Derek4aty1 <derekhuber.huber@gmail.com>
2024-07-21 14:33:04 -0500

    Misc Formatting

--- src/components/BrawlerPortrait.tsx
+++ src/components/BrawlerPortrait.tsx
@@ -1,53 +1,53 @@
 import Image from "next/image";
 
 export type PortraitData = {
-    name: string,
-    path: string,
-    rarity: BrawlerRarity
-  };
-  
-  export enum BrawlerRarity {
-    TrophyRoad,
-    Rare,
-    SuperRare,
-    Epic,
-    Mythic,
-    Legendary
-  };
+  name: string,
+  path: string,
+  rarity: BrawlerRarity
+};
 
-export default function BrawlerPortrait({portrait}: {portrait: PortraitData}) {
-    function getBackgroundColor(rarity: BrawlerRarity) {
-        switch (rarity) {
-            case BrawlerRarity.TrophyRoad:
-                return '#94d7f4';
-            case BrawlerRarity.Rare:
-                return '#2edd1b';
-            case BrawlerRarity.SuperRare:
-                return '#0087fa';
-            case BrawlerRarity.Epic:
-                return '#b116ec';
-            case BrawlerRarity.Mythic:
-                return '#ff0021';
-            case BrawlerRarity.Legendary:
-                return '#fff11e';
-        }
+export enum BrawlerRarity {
+  TrophyRoad,
+  Rare,
+  SuperRare,
+  Epic,
+  Mythic,
+  Legendary
+};
+
+export default function BrawlerPortrait({ portrait }: { portrait: PortraitData }) {
+  function getBackgroundColor(rarity: BrawlerRarity) {
+    switch (rarity) {
+      case BrawlerRarity.TrophyRoad:
+        return '#94d7f4';
+      case BrawlerRarity.Rare:
+        return '#2edd1b';
+      case BrawlerRarity.SuperRare:
+        return '#0087fa';
+      case BrawlerRarity.Epic:
+        return '#b116ec';
+      case BrawlerRarity.Mythic:
+        return '#ff0021';
+      case BrawlerRarity.Legendary:
+        return '#fff11e';
     }
+  }
 
-    return (
-        <figure className="flex flex-col items-center gap-2">
-            <div className="relative w-[160px] h-[100px]">
-              <Image
-                src={portrait.path}
-                alt={portrait.name}
-                fill={true}
-                sizes="(max-width: 768px) 100vw, 50vw"
-                style={{ objectFit: "contain", objectPosition: "left", backgroundColor: getBackgroundColor(portrait.rarity) }}
-                className="border-[3px] border-black shadow-2xl rounded"
-              />
-              <figcaption className="absolute px-[6px] bottom-0 right-0 text-center text-white text-sm text-shadow-outline leading-6">
-                {portrait.name}
-              </figcaption>
-            </div>
-          </figure>
-    )
+  return (
+    <figure className="flex flex-col items-center gap-2">
+      <div className="relative w-[160px] h-[100px]">
+        <Image
+          src={portrait.path}
+          alt={portrait.name}
+          fill={true}
+          sizes="(max-width: 768px) 100vw, 50vw"
+          style={{ objectFit: "contain", objectPosition: "left", backgroundColor: getBackgroundColor(portrait.rarity) }}
+          className="border-[3px] border-black shadow-2xl rounded"
+        />
+        <figcaption className="absolute px-[6px] bottom-0 right-0 text-center text-white text-sm text-shadow-outline leading-6">
+          {portrait.name}
+        </figcaption>
+      </div>
+    </figure>
+  )
 }
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
-    return (
-        <footer className="flex flex-col flex-wrap items-center justify-center gap-4 px-8 pb-4 text-center text-sm">
-            <hr className="border-white border-t-2 w-full" style={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.5)' }} />
-            <p>2024 Brawlstars Explorer - This material is unofficial and is not endorsed by Supercell. For more information see Supercell&apos;s Fan Content Policy:
-                <a href="https://supercell.com/en/fan-content-policy/" target="_blank"> https://supercell.com/en/fan-content-policy/.</a>
-            </p>
-        </footer>
-    );
+  return (
+    <footer className="flex flex-col flex-wrap items-center justify-center gap-4 px-8 pb-4 text-center text-sm">
+      <hr className="border-white border-t-2 w-full" style={{ boxShadow: '0 2px 4px rgba(0, 0, 0, 0.5)' }} />
+      <p>2024 Brawlstars Explorer - This material is unofficial and is not endorsed by Supercell. For more information see Supercell&apos;s Fan Content Policy:
+        <a href="https://supercell.com/en/fan-content-policy/" target="_blank"> https://supercell.com/en/fan-content-policy/.</a>
+      </p>
+    </footer>
+  );
 }
