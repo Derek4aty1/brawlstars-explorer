2f2f654380e723481cd097298d5a764cdbb8533b
Derek4aty1 <derekhuber.huber@gmail.com>
2024-07-27 23:30:44 -0500

    Next.js Link Navigation

--- src/components/PortraitCard.tsx
+++ src/components/PortraitCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { BrawlerRarity, PortraitData } from "@/types/BrawlerTypes";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { useInView } from "react-intersection-observer";
 
 const colorVariants = {
@@ -14,7 +14,6 @@ const colorVariants = {
 };
 
 export default function PortraitCard({ portrait }: { portrait: PortraitData }) {
-  const router = useRouter()
   const { ref, inView } = useInView({
     triggerOnce: true
   });
@@ -23,21 +22,24 @@ export default function PortraitCard({ portrait }: { portrait: PortraitData }) {
   const objectFit = portrait.name === 'PEARL' ? 'cover' : 'contain';
 
   return (
-    <figure onClick={() => router.push(`/${portrait.name}`)} className="flex flex-col items-center gap-2 border-[3px] border-transparent rounded-md transition-transform 
+    <Link href={`/${portrait.name}`} prefetch={false}>
+      <figure className="flex flex-col items-center gap-2 border-[3px] border-transparent rounded-md transition-transform 
                        hover:scale-105 hover:border-white hover:bg-white">
-      <div ref={ref} className={`relative w-[160px] aspect-[16/10] border-[3px] border-black shadow-2xl rounded ${colorVariants[portrait.rarity]}`}>
-        <Image
-          src={portrait.path}
-          alt={`${portrait.name} portrait`}
-          fill={true}
-          sizes="(max-width: 768px) 100vw, 50vw"
-          style={{ objectFit: objectFit, objectPosition: "left" }}
-          className={`transition-opacity ease-linear ${inView ? 'opacity-100' : 'opacity-0'}`}
-        />
-        <figcaption className="absolute px-1 bottom-0 right-0 text-sm leading-5">
-          {portrait.name}
-        </figcaption>
-      </div>
-    </figure>
-  )
+        <div ref={ref} className={`relative w-[160px] aspect-[16/10] border-[3px] border-black shadow-2xl rounded ${colorVariants[portrait.rarity]}`}>
+          <Image
+            src={portrait.path}
+            alt={`${portrait.name} portrait`}
+            fill={true}
+            draggable={false}
+            sizes="(max-width: 768px) 100vw, 50vw"
+            style={{ objectFit: objectFit, objectPosition: "left" }}
+            className={`select-none transition-opacity ease-linear ${inView ? 'opacity-100' : 'opacity-0'}`}
+          />
+          <figcaption className="absolute px-1 bottom-0 right-0 text-sm leading-5">
+            {portrait.name}
+          </figcaption>
+        </div>
+      </figure>
+    </Link>
+  );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,30 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="z-50 sticky top-0 bg-black h-[64px] flex">
       <nav className="flex w-full">
-        <a href="/" className="m-3 flex transition duration-200 hover:scale-110 w-auto">
-          <Image src="/images/brawlstars-logo-wings.png" alt="Brawlstars logo with wings" width={50} height={50} style={{ objectFit: "contain" }} />
-        </a>
-        <a href="https://github.com/Derek4aty1/brawlstars-explorer" target="_blank" className="m-3 ml-auto flex items-center transition duration-200 hover:scale-110">
-          <Image src="/images/github-logo-white.svg" alt="Github logo" width={30} height={30} />
+        <Link href="/" draggable={false} className="m-3 flex transition duration-200 hover:scale-110 w-auto">
+          <Image
+            src="/images/brawlstars-logo-wings.png"
+            alt="Brawlstars logo with wings"
+            width={50}
+            height={50}
+            draggable={false}
+            style={{ objectFit: "contain" }}
+            className="select-none"
+          />
+        </Link>
+        <a href="https://github.com/Derek4aty1/brawlstars-explorer" target="_blank" draggable={false} className="m-3 ml-auto flex items-center transition duration-200 hover:scale-110">
+          <Image
+            src="/images/github-logo-white.svg"
+            alt="Github logo"
+            width={30}
+            height={30}
+            draggable={false}
+            className="select-none"
+          />
         </a>
       </nav>
     </header>
